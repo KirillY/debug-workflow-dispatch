423d94b152ce51dbd9fb4bb2c0eb0747bf4c6fb8
Kirill Lapshin <2flashback@gmail.com>
2024-07-24 20:18:20 -0300

    Update newman.sh

--- newman.sh
+++ newman.sh
@@ -1,5 +1,5 @@
-POSTMAN_COLLECTION_URL="https://api.getpostman.com/collections/$MORPHEUS_POSTMAN_COLLECTION_UID?apikey=$MORPHEUS_POSTMAN_API_KEY"
-POSTMAN_ENV_URL="https://api.getpostman.com/environments/$MORPHEUS_POSTMAN_ENV_UID?apikey=$MORPHEUS_POSTMAN_API_KEY"
+POSTMAN_COLLECTION_URL="https://api.getpostman.com/collections/$POSTMAN_COLLECTION_UID?apikey=$POSTMAN_API_KEY"
+POSTMAN_ENV_URL="https://api.getpostman.com/environments/$POSTMAN_ENV_UID?apikey=$POSTMAN_API_KEY"
 
 newman run $POSTMAN_COLLECTION_URL \
     -e "$POSTMAN_ENV_URL"
